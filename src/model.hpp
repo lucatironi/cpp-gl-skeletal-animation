db6f7431f5b84f3f3e5e11fa0c6b383d4214e7d4
Luca Tironi <luca.tironi@gmail.com>
2021-07-13 18:43:19 +0200

    Comments refactoring

--- src/model.hpp
+++ src/model.hpp
@@ -35,7 +35,6 @@ unsigned int TextureFromFile(const char* filename, const std::string& directory,
 class Model
 {
     public:
-        // constructor, expects a filepath to a 3D model.
         Model() : animDuration(0.0), currentAnimation(0), bonesCount(0)
         {
             scene = nullptr;
@@ -147,7 +146,9 @@ class Model
             for (unsigned int i = 0; i < mesh->mNumVertices; i++)
             {
                 Vertex vertex;
-                glm::vec3 vector; // we declare a placeholder vector since assimp uses its own vector class that doesn't directly convert to glm's vec3 class so we transfer the data to this placeholder glm::vec3 first.
+                // we declare a placeholder vector since assimp uses its own vector class that doesn't directly convert
+                // to glm's vec3 class so we transfer the data to this placeholder glm::vec3 first.
+                glm::vec3 vector;
                 // positions
                 vector.x = mesh->mVertices[i].x;
                 vector.y = mesh->mVertices[i].y;
